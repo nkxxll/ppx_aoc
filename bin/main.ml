55f1002a0ee15c3620c97dab71c1ec367f2e105e
Niklas Kirschall <niklas@kirschall.com>
2025-09-07 22:50:01 +0200

    have first working deriver

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,10 @@
-let () = print_endline "Hello, World!"
+type aoc_days =
+  { name : string
+  ; age : int
+  }
+[@@deriving aocmatch]
+
+let () =
+  let ad = { name = "tom"; age = 10 } in
+  Printf.printf "%s\n" (name ad)
+;;
