670997f0afa3aa584a2a867f9c74aa57a7998ffa
nkxxll <niki.nice1203@gmail.com>
2025-09-08 18:39:01 +0200

    after a long fight with chatty and my I have a working deriver

--- bin/main.ml
+++ bin/main.ml
@@ -1,10 +1,14 @@
-type aoc_days =
-  { name : string
-  ; age : int
+type aoc_record =
+  { day1 : (unit -> unit) * (unit -> unit)
+  ; day2 : (unit -> unit) * (unit -> unit)
   }
 [@@deriving aocmatch]
 
+let func number = fun () -> print_endline ("hello" ^ string_of_int number)
+let aoc_record = { day1 = func 11, func 12; day2 = func 21, func 22 }
+
 let () =
-  let ad = { name = "tom"; age = 10 } in
-  Printf.printf "%s\n" (name ad)
+  let day = 1 in
+  let task = None in
+  run_aoc aoc_record day task
 ;;
--- lib/ppx_aoc.ml
+++ lib/ppx_aoc.ml
@@ -14,44 +14,102 @@ open Ast_builder.Default
     ```
 *)
 
-let match_impl (ld : label_declaration) =
-  let loc = ld.pld_loc in
+(* TODO if the name is not day<number> it should not be passed to this function *)
+let make_cases ~loc (field : label_declaration) =
+  let name = field.pld_name.txt in
+  (* Extract day number from "dayN" *)
+  let day_number = Scanf.sscanf name "day%d" (fun n -> n) in
+  let day_expr =
+    pexp_field
+      ~loc
+      (evar ~loc "aoc_record") (* the record expression, e.g., a variable "record" *)
+      (Located.lident ~loc name)
+  in
+  (* Case 1: Some 1 *)
+  let case1 =
+    case
+      ~lhs:
+        (ppat_tuple
+           ~loc
+           [ ppat_constant ~loc (Pconst_integer (string_of_int day_number, None))
+           ; ppat_construct
+               ~loc
+               (Located.lident ~loc "Some")
+               (Some (ppat_constant ~loc (Pconst_integer ("1", None))))
+           ])
+      ~guard:None
+      ~rhs:
+        [%expr
+          let task1, _ = [%e day_expr] in
+          task1 ()]
+  in
+  (* Case 2: Some 2 *)
+  let case2 =
+    case
+      ~lhs:
+        (ppat_tuple
+           ~loc
+           [ ppat_constant ~loc (Pconst_integer (string_of_int day_number, None))
+           ; ppat_construct
+               ~loc
+               (Located.lident ~loc "Some")
+               (Some (ppat_constant ~loc (Pconst_integer ("2", None))))
+           ])
+      ~guard:None
+      ~rhs:
+        [%expr
+          let _, task2 = [%e day_expr] in
+          task2 ()]
+  in
+  (* Case 3: None *)
+  let case3 =
+    case
+      ~lhs:
+        (ppat_tuple
+           ~loc
+           [ ppat_constant ~loc (Pconst_integer (string_of_int day_number, None))
+           ; ppat_construct ~loc (Located.lident ~loc "None") None
+           ])
+      ~guard:None
+      ~rhs:
+        [%expr
+          let task1, task2 = [%e day_expr] in
+          task1 ();
+          task2 ()]
+  in
+  [ case1; case2; case3 ]
+;;
+
+let make_run_aoc ~loc cases =
+  (* match (day, task) with ... *)
+  let match_expr =
+    pexp_match ~loc (pexp_tuple ~loc [ evar ~loc "day"; evar ~loc "task" ]) cases
+  in
+  (* let run_aoc aoc_record day task = match ... *)
   pstr_value
     ~loc
     Nonrecursive
-    [ { pvb_pat = ppat_var ~loc ld.pld_name
-      ; pvb_expr =
-          pexp_fun
-            ~loc
-            Nolabel
-            None
-            (ppat_var ~loc { loc; txt = "x" })
-            (pexp_field
-               ~loc
-               (pexp_ident ~loc { loc; txt = lident "x" })
-               { loc; txt = lident ld.pld_name.txt })
-      ; pvb_attributes = []
-      ; pvb_loc = loc
-      ; pvb_constraint = None
-      }
+    [ value_binding
+        ~loc
+        ~pat:(ppat_var ~loc { loc; txt = "run_aoc" })
+        ~expr:
+          (pexp_fun
+             ~loc
+             Nolabel
+             None
+             (ppat_var ~loc { loc; txt = "aoc_record" })
+             (pexp_fun
+                ~loc
+                Nolabel
+                None
+                (ppat_var ~loc { loc; txt = "day" })
+                (pexp_fun
+                   ~loc
+                   Nolabel
+                   None
+                   (ppat_var ~loc { loc; txt = "task" })
+                   match_expr)))
     ]
-;;
-
-let match_intf ~ptype_name (ld : label_declaration) =
-  let loc = ld.pld_loc in
-  psig_value
-    ~loc
-    { pval_name = ld.pld_name
-    ; pval_type =
-        ptyp_arrow
-          ~loc
-          Nolabel
-          (ptyp_constr ~loc { loc; txt = lident ptype_name.txt } [])
-          ld.pld_type
-    ; pval_attributes = []
-    ; pval_loc = loc
-    ; pval_prim = []
-    }
 ;;
 
 let generate_impl ~ctxt (_rec_flag, type_declarations) =
@@ -64,33 +122,25 @@ let generate_impl ~ctxt (_rec_flag, type_declarations) =
           ~loc:ptype_loc
           "Cannot derive accessors for non record types"
       in
-      [ Ast_builder.Default.pstr_extension ~loc ext [] ]
-    | { ptype_kind = Ptype_record fields; _ } -> List.map fields ~f:match_impl)
-  |> List.concat
-;;
-
-let generate_intf ~ctxt (_rec_flag, type_declarations) =
-  let loc = Expansion_context.Deriver.derived_item_loc ctxt in
-  List.map type_declarations ~f:(fun (td : type_declaration) ->
-    match td with
-    | { ptype_kind = Ptype_abstract | Ptype_variant _ | Ptype_open; ptype_loc; _ } ->
-      let ext =
-        Location.error_extensionf
-          ~loc:ptype_loc
-          "Cannot derive accessors for non record types"
+      Ast_builder.Default.pstr_extension ~loc ext []
+    | { ptype_kind = Ptype_record fields; _ } ->
+      let cases =
+        List.filter_map fields ~f:(fun field ->
+          match field.pld_type.ptyp_desc with
+          | Ptyp_tuple _ -> Some (make_cases ~loc field)
+          | _ -> None)
+        @ [ [ case
+                ~lhs:(ppat_any ~loc)
+                ~guard:None
+                ~rhs:[%expr print_endline "No solution for this day-task combination"]
+            ]
+          ]
+        |> List.concat
       in
-      [ Ast_builder.Default.psig_extension ~loc ext [] ]
-    | { ptype_kind = Ptype_record fields; ptype_name; _ } ->
-      List.map fields ~f:(match_intf ~ptype_name))
-  |> List.concat
+      let m = make_run_aoc ~loc cases in
+      m)
 ;;
 
 (* implementation generator *)
 let impl_generator = Deriving.Generator.V2.make_noarg generate_impl
-
-(* interface generator *)
-let intf_generator = Deriving.Generator.V2.make_noarg generate_intf
-
-let my_deriver =
-  Deriving.add "aocmatch" ~str_type_decl:impl_generator ~sig_type_decl:intf_generator
-;;
+let my_deriver = Deriving.add "aocmatch" ~str_type_decl:impl_generator
